27ffe87d7c58a0868dce12c5029de5cd1ff258f3
walterxie <walterxie@users.noreply.github.com>
2025-12-01 14:45:24 +1300

    fix Uniform tests #18

--- src/beast/base/spec/inference/distribution/Uniform.java
+++ src/beast/base/spec/inference/distribution/Uniform.java
@@ -10,15 +10,18 @@ import org.apache.commons.statistics.distribution.UniformContinuousDistribution;
 
 import java.util.List;
 
-
-@Description("Uniform distribution over a given interval (including lower and upper values)")
+/**
+ * bounds do not support Inf and -Inf
+ */
+@Description("Uniform distribution over a given interval (including lower and upper values). " +
+        "An exception is thrown if any bound is set to infinity.")
 public class Uniform extends ScalarDistribution<RealScalar<Real>, Double>
         implements Bounded<Double> {
 
     final public Input<RealScalar<Real>> lowerInput = new Input<>("lower",
-            "lower bound on the interval, default 0");
+            "lower bound on the interval, default 0. An exception is thrown if set to -Infinity");
     final public Input<RealScalar<Real>> upperInput = new Input<>("upper",
-            "upper bound on the interval, default 1");
+            "upper bound on the interval, default 1. An exception is thrown if set to Infinity");
 
     // if (!Double.isFinite(upper - lower)) {
     //    throw new DistributionException("Range %s is not finite", upper - lower);
@@ -31,6 +34,12 @@ public class Uniform extends ScalarDistribution<RealScalar<Real>, Double>
      */
     public Uniform() {}
 
+    /**
+     * Uniform distribution over a given interval (including lower and upper values)
+     * @param param
+     * @param lower An exception is thrown if set to -Infinity.
+     * @param upper An exception is thrown if set to Infinity.
+     */
     public Uniform(RealScalar<Real> param,
                 RealScalar<Real> lower, RealScalar<Real> upper) {
 
