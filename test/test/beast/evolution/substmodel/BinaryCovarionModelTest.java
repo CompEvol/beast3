6ba8d609969a0a4f2c5bd985b91ea55b185899b3
rbouckaert <higgs.ml@gmail.com>
2026-01-22 14:25:03 +1300

    fix BinaryCovarionModelTest. fixes #32

--- test/test/beast/evolution/substmodel/BinaryCovarionModelTest.java
+++ test/test/beast/evolution/substmodel/BinaryCovarionModelTest.java
@@ -2,16 +2,21 @@ package test.beast.evolution.substmodel;
 
 import org.junit.jupiter.api.Test;
 
-import beast.base.evolution.substitutionmodel.BinaryCovarion;
-import beast.base.evolution.substitutionmodel.Frequencies;
-import beast.base.inference.parameter.RealParameter;
+import beast.base.spec.domain.PositiveReal;
+import beast.base.spec.domain.UnitInterval;
+import beast.base.spec.evolution.substitutionmodel.BinaryCovarion;
+import beast.base.spec.evolution.substitutionmodel.Frequencies;
+import beast.base.spec.inference.parameter.RealScalarParam;
+import beast.base.spec.inference.parameter.RealVectorParam;
+import beast.base.spec.inference.parameter.SimplexParam;
+import beast.base.spec.type.RealVector;
+import beast.base.spec.type.Simplex;
 import beast.base.util.Randomizer;
 import cern.colt.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinaryCovarionModelTest  {
     final static double EPSILON = 1e-6;
-
 	
     /**      
 	 * test that equilibrium frequencies are
@@ -19,7 +24,7 @@ public class BinaryCovarionModelTest  {
 	 */
 	@Test
 	public void testEquilibriumFrequencies() {
-		Randomizer.setSeed(120);
+		Randomizer.setSeed(127);
 		doWithTufflySteel();
 
 		for (int i = 0; i < 10; i++) {
@@ -31,30 +36,31 @@ public class BinaryCovarionModelTest  {
 	
 	private void doWithTufflySteel() {
 	        Frequencies dummyFreqs = new Frequencies();
-	        dummyFreqs.initByName("frequencies", "0.25 0.25 0.25 0.25", "estimate", false);
+	        Simplex frequencies = new SimplexParam(new double[]{0.25,0.25,0.25,0.25});
+	        dummyFreqs.initByName("frequencies", frequencies, "estimate", false);
 	        BinaryCovarion substModel;
 
 	        double d = 0.05+Randomizer.nextDouble()*0.9;
-	        RealParameter vfrequencies = new RealParameter(new Double[]{d, 1.0 - d});
-	        RealParameter switchrate = new RealParameter(new Double[]{Randomizer.nextDouble(), Randomizer.nextDouble()});
+	        Simplex vfrequencies = new SimplexParam(new double[]{d, 1.0 - d});
+	        RealVector<UnitInterval> switchrate = new RealVectorParam<>(new double[]{Randomizer.nextDouble(), Randomizer.nextDouble()}, UnitInterval.INSTANCE);
 	        
 	        substModel = new BinaryCovarion();
 	        substModel.initByName("frequencies", dummyFreqs, 
 	        		"vfrequencies", vfrequencies, /* [p0, p1] */
-	        		"alpha", "0.01",
+	        		"alpha", new RealScalarParam<PositiveReal>(0.01, PositiveReal.INSTANCE),
 	        		"switchRate", switchrate,
 	        		//"eigenSystem", "beast.evolution.substitutionmodel.RobustEigenSystem",
 	        		"mode", "TUFFLEYSTEEL");
 	        
 	        double [] matrix = new double[16];
 	        substModel.getTransitionProbabilities(null, 1000, 0, 1.0, matrix);
-        	double h0 = switchrate.getValue(1) / (switchrate.getValue(0) + switchrate.getValue(1));
-        	double h1 = switchrate.getValue(0) / (switchrate.getValue(0) + switchrate.getValue(1));
+        	double h0 = switchrate.get(1) / (switchrate.get(0) + switchrate.get(1));
+        	double h1 = switchrate.get(0) / (switchrate.get(0) + switchrate.get(1));
 	        double [] baseFreqs = new double[] {
-	    	        vfrequencies.getValue(0) * h0,
-	    	        vfrequencies.getValue(1) * h0,
-	    	        vfrequencies.getValue(0) * h1,
-	    	        vfrequencies.getValue(1) * h1
+	    	        vfrequencies.get(0) * h0,
+	    	        vfrequencies.get(1) * h0,
+	    	        vfrequencies.get(0) * h1,
+	    	        vfrequencies.get(1) * h1
 	        };
 	        System.err.println("Expected: " + Arrays.toString(baseFreqs));
 	        System.err.println("Calculat: " + Arrays.toString(matrix));
@@ -65,56 +71,58 @@ public class BinaryCovarionModelTest  {
 
 	private void doWithEqualHFreqs(String mode) {
         Frequencies dummyFreqs = new Frequencies();
-        dummyFreqs.initByName("frequencies", "0.25 0.25 0.25 0.25", "estimate", false);
+        Simplex frequencies = new SimplexParam(new double[]{0.25,0.25,0.25,0.25});
+        dummyFreqs.initByName("frequencies", frequencies, "estimate", false);
         BinaryCovarion substModel;
 
-        RealParameter hfrequencies = new RealParameter(new Double[]{0.5, 0.5});
+        Simplex hfrequencies = new SimplexParam(new double[]{0.5, 0.5});
         double d = Randomizer.nextDouble();
-        RealParameter vfrequencies = new RealParameter(new Double[]{d, 1.0 - d});
+        Simplex vfrequencies = new SimplexParam(new double[]{d, 1.0 - d});
         
         substModel = new BinaryCovarion();
         substModel.initByName("frequencies", dummyFreqs, 
         		"hfrequencies", hfrequencies, /* [f0, f1] */
         		"vfrequencies", vfrequencies, /* [p0, p1] */
-        		"alpha", "0.01",
-        		"switchRate", "0.1",
+        		"alpha", new RealScalarParam<PositiveReal>(0.01, PositiveReal.INSTANCE),
+        		"switchRate", new RealVectorParam<PositiveReal>(new double[] {0.1}, PositiveReal.INSTANCE) {},
         		"mode", mode);
         
         double [] matrix = new double[16];
         substModel.getTransitionProbabilities(null, 100, 0, 1.0, matrix);
         double EPSILON = 1e-10;
-        assertEquals(vfrequencies.getValue(0) * hfrequencies.getValue(0), matrix[0], EPSILON);
-        assertEquals(vfrequencies.getValue(1) * hfrequencies.getValue(0), matrix[1], EPSILON);
-        assertEquals(vfrequencies.getValue(0) * hfrequencies.getValue(1), matrix[2], EPSILON);
-        assertEquals(vfrequencies.getValue(1) * hfrequencies.getValue(1), matrix[3], EPSILON);
+        assertEquals(vfrequencies.get(0) * hfrequencies.get(0), matrix[0], EPSILON);
+        assertEquals(vfrequencies.get(1) * hfrequencies.get(0), matrix[1], EPSILON);
+        assertEquals(vfrequencies.get(0) * hfrequencies.get(1), matrix[2], EPSILON);
+        assertEquals(vfrequencies.get(1) * hfrequencies.get(1), matrix[3], EPSILON);
 	}
 
 	private void doWithUnEqualHFreqs(String mode) {
         Frequencies dummyFreqs = new Frequencies();
-        dummyFreqs.initByName("frequencies", "0.25 0.25 0.25 0.25", "estimate", false);
+        Simplex frequencies = new SimplexParam(new double[]{0.25,0.25,0.25,0.25});
+        dummyFreqs.initByName("frequencies", frequencies, "estimate", false);
         BinaryCovarion substModel;
 
-        double d = 0.05+Randomizer.nextDouble()*0.9;
-        RealParameter hfrequencies = new RealParameter(new Double[]{d, 1.0 - d});
-        d = 0.05+Randomizer.nextDouble()*0.9;
-        RealParameter vfrequencies = new RealParameter(new Double[]{d, 1.0 - d});
+        double d = 0.05 + Randomizer.nextDouble()*0.9;
+        Simplex hfrequencies = new SimplexParam(new double[]{d, 1.0 - d});
+        d = 0.05 + Randomizer.nextDouble()*0.9;
+        Simplex vfrequencies = new SimplexParam(new double[]{d, 1.0 - d});
         
         substModel = new BinaryCovarion();
         substModel.initByName("frequencies", dummyFreqs, 
         		"hfrequencies", hfrequencies, /* [f0, f1] */
         		"vfrequencies", vfrequencies, /* [p0, p1] */
-        		"alpha", "0.01",
-        		"switchRate", "0.1",
-        		//"eigenSystem", "beast.evolution.substitutionmodel.RobustEigenSystem",
+        		"alpha", new RealScalarParam<PositiveReal>(0.01, PositiveReal.INSTANCE),
+        		"switchRate", new RealVectorParam<PositiveReal>(new double[] {0.1}, PositiveReal.INSTANCE) {},
+        		"eigenSystem", "beast.base.evolution.substitutionmodel.ColtEigenSystem",
         		"mode", mode);
         
         double [] matrix = new double[16];
         substModel.getTransitionProbabilities(null, 1000, 0, 1.0, matrix);
         double [] baseFreqs = new double[] {
-	        (vfrequencies.getValue(0) * hfrequencies.getValue(0)),
-	        (vfrequencies.getValue(1) * hfrequencies.getValue(0)),
-	        (vfrequencies.getValue(0) * hfrequencies.getValue(1)),
-	        (vfrequencies.getValue(1) * hfrequencies.getValue(1))
+	        (vfrequencies.get(0) * hfrequencies.get(0)),
+	        (vfrequencies.get(1) * hfrequencies.get(0)),
+	        (vfrequencies.get(0) * hfrequencies.get(1)),
+	        (vfrequencies.get(1) * hfrequencies.get(1))
         };
         System.err.println("Expected: " + Arrays.toString(baseFreqs));
         System.err.println("Calculat: " + Arrays.toString(matrix));
