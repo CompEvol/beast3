8564a1fdaa21d46d46b15f8f40543a3f461fb8b8
Alexei Drummond <alexei.drummond@gmail.com>
2026-04-09 08:33:11 -0600

    Release v2.8.0-beta4

--- beast-base/src/main/java/beast/base/core/BEASTVersion2.java
+++ beast-base/src/main/java/beast/base/core/BEASTVersion2.java
@@ -26,7 +26,7 @@ public class BEASTVersion2 extends BEASTVersion {
      * Pre-release label appended to the version string (e.g. "beta1", "rc1").
      * Set to {@code null} for a final release.
      */
-    private static final String PRERELEASE = "beta3";
+    private static final String PRERELEASE = "beta4";
 
     @Override
 	public String getVersion() {
